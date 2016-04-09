fa217bebfbc7d5f0246053befbc38ece9de3a975
徐章 <852693851@qq.com>
2016-04-09 13:17:49 +0800

    1.create project

---
+++ XZCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZCategory
+//
+//  Created by 徐章 on 16/4/9.
+//  Copyright © 2016年 徐章. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
